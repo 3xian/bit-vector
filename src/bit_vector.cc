d1cc16009384e34aac0931d97f8bda27b3ecc9a9
root <root@woo-weixin.(none)>
2013-07-29 00:30:15 +0800

    .

--- src/bit_vector.cc
+++ src/bit_vector.cc
@@ -15,6 +15,8 @@ void BitVector::Init(size_t size) {
     size_ = size;
     size_t blocks_size = (size + kBlockSize - 1) / kBlockSize;
     blocks_ = std::vector<uint64_t>(blocks_size, 0);
+
+    //TODO
 }
 
 void BitVector::Clear() {
@@ -27,7 +29,11 @@ size_t BitVector::Size() const {
 }
 
 size_t BitVector::Count() const {
-
+    if (size_ == 0) {
+        return 0;
+    } else {
+        return FenwickSum(fenwick_.size() - 1);
+    }
 }
 
 void BitVector::PushBack(int bit) {
@@ -38,11 +44,11 @@ void BitVector::PopBack() {
 
 }
 
-int BitVector::operator [](size_t index) const {
-
+int BitVector::operator [](size_t id) const {
+    return blocks_[id / kBlockSize] >> (id & (kBlockSize - 1)) & 1;
 }
 
-void BitVector::SetBit(size_t index, int bit) {
+void BitVector::SetBit(size_t id, int bit) {
 
 }
 
@@ -62,6 +68,16 @@ std::string BitVector::ToString() const {
         }
     }
     return str;
+}
+
+int BitVector::FenwickSum(int i) const {
+    int s = 0;
+    for (; p > 0; p -= p & -p) s += _c[p];
+    return s;
+}
+
+void BitVector::FenwickUpdate(int id, int delta) {
+    for (; p <= _n; p += p & -p) _c[p] += d;
 }
 
 }  // namespace seiya
--- src/bit_vector.h
+++ src/bit_vector.h
@@ -19,8 +19,8 @@ public:
     void PushBack(int bit);
     void PopBack();
 
-    int operator [](size_t index) const;
-    void SetBit(size_t index, int bit);
+    int operator [](size_t id) const;
+    void SetBit(size_t id, int bit);
 
     uint64_t Select(uint64_t bit, uint64_t rank) const;
     uint64_t Rank(uint64_t bit, uint64_t pos) const;
@@ -30,9 +30,12 @@ public:
 private:
     static const size_t kBlockSize = 64;
 
+    int FenwickSum(int id) const;
+    void FenwickUpdate(int id, int delta);
+
     size_t size_;
     std::vector<uint64_t> blocks_;
-    std::vector<uint32_t> fenwick_;
+    std::vector<int> fenwick_;
 };
 
 }  // namespace seiya
